ec3c2c097aaf9a8dbb7fba3f65245091816c7fc3
mamylahi <dembouzfaye7@gmail.com>
2026-06-12 09:24:52 +0000

    chore(terraform): Remove old Kubernetes-only modules (replaced by AWS modules)

--- terraform/modules/namespace/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "Nom du namespace Kubernetes créé pour le portfolio"
-  value       = kubernetes_namespace.this.metadata[0].name
-}
-
-output "id" {
-  description = "ID unique du namespace"
-  value       = kubernetes_namespace.this.id
-}
--- terraform/modules/namespace/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "namespace" {
-  description = "Nom du namespace Kubernetes pour isoler les ressources du portfolio"
-  type        = string
-
-  validation {
-    condition     = length(var.namespace) > 0 && length(var.namespace) <= 63
-    error_message = "Le nom du namespace doit contenir entre 1 et 63 caractères."
-  }
-}
